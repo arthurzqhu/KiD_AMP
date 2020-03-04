61909265014c16b6794f173953aaefef1d0c6722
Arthur <azqhu@ucdavis.edu>
2020-03-04 10:21:47 -0700

    flags now record -1/nan when cloud/rain mixing ratio below certain threshold

--- KiD_2.3.2654/src/find_params.f90
+++ KiD_2.3.2654/src/find_params.f90
@@ -467,9 +467,12 @@ End subroutine searchparamsG
 Subroutine searchparams3M(guess,ihyd,md,flag,oMxM3,oMyM3,nMxM3,nMyM3)
 
 use micro_prm, only:relax,Mp,M3p,Mxp,Myp,momx,nkr, &
-               nutab,dntab,minmaxmx,mintab,maxtab,ntab
+               nutab,dntab,minmaxmx,mintab,maxtab,ntab, &
+               cloud_mr_th, rain_mr_th
 use parameters, only:flag_count
 use namelists, only:ovc_factor
+use, intrinsic :: ieee_arithmetic, only: IEEE_Value, IEEE_QUIET_NAN
+use, intrinsic :: iso_fortran_env, only: real32
 
 implicit none
 real(8) :: guess(2),oguess(2),vals(2),ovals(2),tol
@@ -483,6 +486,9 @@ integer, parameter :: lwa=33
 real(8),dimension(lwa) :: wa
 external :: fcn_2p
 real(8), optional :: oMxM3,oMyM3,nMxM3,nMyM3
+real(real32) :: nan
+
+nan = IEEE_VALUE(nan, IEEE_QUIET_NAN)
 
 flag(:) = 0
 !This subroutine drives the search for PDF parameters that satisfy the
@@ -664,6 +670,17 @@ endif
 if (abs(vals(1))>tol) flag(3)=1
 if (abs(vals(2))>tol) flag(4)=1
 if (abs(vals(1))>tol .or. abs(vals(2))>tol) flag(1)=1
+
+!Set flag to -1 or nan if cloud or rain mass didn't reach the threshold
+if ((ihyd==1 .and. M3p<cloud_mr_th) .or. (ihyd==2 .and. M3p<rain_mr_th)) then
+  flag(1) = -1
+  flag(2:flag_count) = nan
+end if
+
+!if (ihyd==2 .and. M3p<1e-7) then
+!  flag(1) = -1
+!  flag(2:flag_count) = nan
+!end if
 
 !Force third moment to have no error and calculate final distribution
 !print*,'a',guess
