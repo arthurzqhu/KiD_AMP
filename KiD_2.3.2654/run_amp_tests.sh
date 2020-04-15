a099848e2775a83e5b021aea3ffc762f78a66fb5
Arthur <azqhu@ucdavis.edu>
2020-04-14 19:21:38 -0600

    compiled

--- KiD_2.3.2654/run_amp_tests.sh
+++ KiD_2.3.2654/run_amp_tests.sh
@@ -1,3 +1,52 @@
+#!/bin/bash
+
+caselist=(101 102 103 105 106 107)
+case_num=${#caselist[@]}
+
+
+icimm=0.0
+icinm=0.0
+isp_c=4  # shape parameter for cloud
+isp_r=4  # shape parameter for rain
+imc1=0 # II moment for cloud
+imc2=6 # III moment for cloud
+imr1=0 # II moment for rain
+imr2=6 # III moment for rain
+ia=300
+
+#for icimm in 0.0 0.0003 0.001 0.003 #initial cloud mass mixing ratio
+#do
+	#for icinm in 0 30 100 300 #initial number mixing ratio
+	#do
+	
+#for isp_c in 2 15
+#do
+#	for isp_r in 2 15
+#	do
+
+#mc1=(0 2 2 4 4 6)
+#mc2=(2 0 4 2 6 4)
+#mnum=${#mc1[@]}
+
+#for ((imnum=0; imnum<mnum; imnum++))
+#do
+#	imc1=${mc1[imnum]}
+#	imc2=${mc2[imnum]}
+#for ((imc1=0; imc1<8; imc1=imc1+2))
+#do
+#	for ((imc2=imc1+2; imc2<=8; imc2=imc2+2))
+#	do
+#		echo $imc1 $imc2
+		for ((ic=0; ic<case_num; ic++))
+		do
+			if [ ${caselist[ic]} -gt 104 ] && [ ${caselist[ic]} -lt 200 ]
+			then
+				zc=0
+			else
+				zc="3000.,600.,900."
+			fi
+			echo “${caselist[ic]}”
+			cat > namelists/AMP.nml <<END
 
 &mphys
 ! hydrometeor names
@@ -7,10 +56,10 @@ h_names='cloud','rain'
 mom_names='M1','M2','M3'
 
 !Initial shape parameter
-h_shape=4,4
+h_shape=$isp_c,$isp_r
 
 !Initial cloud mixing ratio (kg/kg) and number mixing ratio (#/kg)
-cloud_init=0.0,0.0  !.001,100.e6
+cloud_init=$icimm,$icinm  !.001,100.e6
 
 !Initial rain mixing ratio (kg/kg) and number mixing ratio (#/kg)
 rain_init=0.0,0.0
@@ -36,7 +85,7 @@ dosedimentation = .true.
 ! Aerosol initialization
 num_aero_moments=0
 num_aero_bins=1
-aero_N_init=300.e6 !or CCN at 1% SS
+aero_N_init=$ia.e6 !or CCN at 1% SS
 aero_sig_init=1.4
 aero_rd_init=0.05e-6
 
@@ -47,7 +96,7 @@ mom_init=0,0,0
 /
 
 &case
-icase=107
+icase=${caselist[ic]}
 /
 
 &control
@@ -60,7 +109,7 @@ tctrl(1)=3600.    !Total length of simulation (s)
 tctrl(2)=600.     !May not be used, depends on the case. Typically the period of w oscillation
 tctrl(3)=1080.	  !For cases 105-107
 tctrl(4)=1200.    !For cases 105-107
-zctrl=0 !zctrl(1) is the domain height, (2) and (3) specify the location to init. hydromets.
+zctrl=$zc !zctrl(1) is the domain height, (2) and (3) specify the location to init. hydromets.
 /
 
 &switch
@@ -77,5 +126,12 @@ l_periodic_bound=.False.
 /
 
 &addcontrol
-KiD_outdir='/glade/scratch/arthurhu/KiD_AMP_output/'
+KiD_outdir='/glade/scratch/$USER/KiD_AMP_output/'
 /
+END
+		./bin/KiD_1D.exe namelists/AMP.nml
+				#done
+			#done
+		done
+#	done
+#done
