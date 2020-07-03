2c1140d2f36a54d4118fa6eb4de0ef0ebef78ac1
Arthur <ols.Arthur@gmail.com>
2020-07-03 16:40:15 -0700

    what if i simply move where sedimentation routine is called

--- KiD_2.3.2654/src/module_mp_tau_bin.f90
+++ KiD_2.3.2654/src/module_mp_tau_bin.f90
@@ -202,12 +202,6 @@ module module_mp_tau_bin
         ENDDO
      ENDDO
 
-     if (l_sediment .or. dosedimentation) then
-
-        CALL BIN_SEDIMENT(I,DT,AMKORIG,ANKORIG,Q,SQ,RDT)
-
-     endif                    ! sedimentation calculation
-
      totevap = 0.0
      totevap2 = 0.0
      totccnreg = 0.0
@@ -379,6 +373,12 @@ do j=jminp,jmaxp
         enddo
     enddo
 enddo
+
+if (l_sediment .or. dosedimentation) then
+
+   CALL BIN_SEDIMENT(I,DT,AMKORIG,ANKORIG,Q,SQ,RDT)
+
+endif                    ! sedimentation calculation
 
      END subroutine TAU_BIN
 
