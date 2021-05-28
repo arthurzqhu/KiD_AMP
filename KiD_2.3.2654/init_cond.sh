f260a0c9524c7f53d6897fd6d71b03342d11c9ac
Arthur Hu <ols.Arthur@gmail.com>
2021-05-28 04:41:32 -0700

    converts to zsh, changes simulation time based on updraft speed to prevent overshooting

--- KiD_2.3.2654/init_cond.sh
+++ KiD_2.3.2654/init_cond.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 
 # config of the run
 mconfig='noinit' # case/folder name. determined automatically if set empty
@@ -21,7 +21,9 @@ imc1=0 # II moment for cloud
 imc2=6 # III moment for cloud
 imr1=0 # II moment for rain
 imr2=6 # III moment for rain
-
+ztop=6000. # top of the domain
+t1=3600.
+t2=900.
 # switches
 l_nuc_cond_s=1
 l_coll_s=1
@@ -82,18 +84,26 @@ fi
 #	for ((imc2=imc1+2; imc2<=8; imc2=imc2+2))
 #	do
 
-for iw in 0.5 1 2 4 6
+for iw in 8 10 #0.5 1 2 4 6 8 10
 do
-echo w=$iw
+  echo w=$iw
+  # reset oscillation time based on updraft speed to prevent overshooting
+  if [[ $((ztop/$iw)) -lt $t2 ]]; then
+    t2=$((ztop/$iw))
+    t1=$(($t2*4))
+  fi
+  echo t1=$t1
+  echo t2=$t2
+
   for ia in 50 100 200 400
   do
   echo Na=$ia
-    for ((iab=0; iab<${#ampORbin[@]}; iab=iab+1))
+    for ((iab=1; iab<=${#ampORbin[@]}; iab=iab+1))
     do
-      for ((ibt=0; ibt<${#bintype[@]}; ibt=ibt+1))
+      for ((ibt=1; ibt<=${#bintype[@]}; ibt=ibt+1))
       do
-    	echo ${ampORbin[$iab]}-${bintype[$ibt]}
-        if [ ${ampORbin[$iab]} = 'AMP' ]; then
+    	echo "${ampORbin[$iab]}"-"${bintype[$ibt]}"
+        if [[ ${ampORbin[$iab]} = 'AMP' ]]; then
           nhm='3,3'
           nhb='1,1'
           # changes nhm based on the input 
@@ -104,7 +114,7 @@ echo w=$iw
             nhm=${nhm//,3/$',2'}
           fi
           else
-            if [ ${bintype[$ibt]} = 'SBM' ]; then
+            if [[ ${bintype[$ibt]} = 'SBM' ]]; then
               nhm='1,1'
               nhb='33,33'
             else
@@ -113,20 +123,19 @@ echo w=$iw
             fi
           fi
           outdir=output/$(date +'%Y-%m-%d')/$mconfig/${ampORbin[$iab]}_${bintype[$ibt]}/a${ia}/w${iw}/
-    	  for ((ic=0; ic<case_num; ic++))
+    	  for ((ic=1; ic<=case_num; ic++))
     	  do
-    	    if [ ${caselist[ic]} -gt 104 ] && [ ${caselist[ic]} -lt 200 ]
+    	    if [[ ${caselist[ic]} -gt 104 ]] && [[ ${caselist[ic]} -lt 200 ]]
     	    then
     	      zc=0
             else
-    	      zc="6000.,600.,1200."
+    	      zc="$ztop,600.,1200."
             fi
     	    if [ ! -d $outdir ]; then
     	      mkdir -p $outdir
     	    fi
     	    echo "${caselist[ic]}"
     	    cat > namelists/${ampORbin[$iab]}_${bintype[$ibt]}.nml << END
-
 &mphys
 ! hydrometeor names
 h_names='cloud','rain'
@@ -135,16 +144,16 @@ h_names='cloud','rain'
 mom_names='M1','M2','M3'
 
 !Initial shape parameter
-h_shape=$isp_c,$isp_r
+h_shape=${isp_c},${isp_r}
 
 !Initial cloud mixing ratio (kg/kg) and number mixing ratio (#/kg)
-cloud_init=$icimm,$icinm  !.001,100.e6
+cloud_init=${icimm},${icinm}  !.001,100.e6
 
 !Initial rain mixing ratio (kg/kg) and number mixing ratio (#/kg)
-rain_init=$irimm,$irinm
+rain_init=${irimm},${irinm}
 
 !Constant rain source mean-mass diameter (m) and number mixing ratio (#/kg)
-rain_source=$rs_dm,$rs_N
+rain_source=${rs_dm},${rs_N}
 
 !Initial supersaturation ratio
 !ss_init=0.1
@@ -152,25 +161,25 @@ rain_source=$rs_dm,$rs_N
 ! number of moments for each species
 !To run AMP as the bin scheme, set num_h_moments = 1 and num_h_bins = 33
 !To run AMP as AMP, set num_h_moments = 2 or 3 and num_h_bins = 1
-num_h_moments=$nhm
-num_h_bins=$nhb
+num_h_moments=${nhm}
+num_h_bins=${nhb}
 
 !AMP control - which moments to predict
-imomc1 = $imc1  !1st predicted cloud moment
-imomc2 = $imc2  !2nd predicted cloud moment (if 3M)
-imomr1 = $imr1  !1st predicted rain moment
-imomr2 = $imr2  !2nd predicted rain moment (if 3M)
+imomc1 = ${imc1}  !1st predicted cloud moment
+imomc2 = ${imc2}  !2nd predicted cloud moment (if 3M)
+imomr1 = ${imr1}  !1st predicted rain moment
+imomr2 = ${imr2}  !2nd predicted rain moment (if 3M)
 
 !Microphysics process control
-donucleation = $l_nuc_cond_f
-docondensation = $l_nuc_cond_f
-docollisions = $l_coll_f
-dosedimentation = $l_sed_f
+donucleation = ${l_nuc_cond_f}
+docondensation = ${l_nuc_cond_f}
+docollisions = ${l_coll_f}
+dosedimentation = ${l_sed_f}
 
 ! Aerosol initialization
 num_aero_moments=1
 num_aero_bins=1
-aero_N_init=$ia.e6 !or CCN at 1% SS
+aero_N_init=${ia}.e6 !or CCN at 1% SS
 aero_sig_init=1.4
 aero_rd_init=0.05e-6
 
@@ -189,18 +198,18 @@ mphys_scheme='amp'
 dt=1.0            !Timestep length (s)
 dgstart=0.0       !When to start diagnostic output
 dg_dt=1.0         !Timestep for diagnostic output
-wctrl(1)=$iw      !Updraft speed
-tctrl(1)=4000.    !Total length of simulation (s)
-tctrl(2)=1000.     !May not be used, depends on the case. Typically the period of w oscillation
+wctrl(1)=${iw}      !Updraft speed
+tctrl(1)=${t1}    !Total length of simulation (s)
+tctrl(2)=${t2}     !May not be used, depends on the case. Typically the period of w oscillation
 tctrl(3)=1080.    !For cases 105-107
 tctrl(4)=1200.    !For cases 105-107
-zctrl=$zc !zctrl(1) is the domain height, (2) and (3) specify the location to init. hydromets.
+zctrl=${zc} !zctrl(1) is the domain height, (2) and (3) specify the location to init. hydromets.
 /
 
 &switch
-l_advect=$l_adv
-l_noadv_qv=$l_noadv_qv
-l_noadv_hydrometeors=$l_noadv_hyd
+l_advect=${l_adv}
+l_noadv_qv=${l_noadv_qv}
+l_noadv_hydrometeors=${l_noadv_hyd}
 l_noadv_theta=.true.
 l_diverge=.false.
 l_nodiv_hydrometeors=.true.
@@ -212,9 +221,10 @@ l_periodic_bound=.False.
 
 &addcontrol
 KiD_outdir='$outdir'
-ampORbin='${ampORbin[$iab],,}'
-bintype='${bintype[$ibt],,}'
+ampORbin='${ampORbin[$iab]:l}'
+bintype='${bintype[$ibt]:l}'
 mp_proc_dg=.true.
+initprof='i'
 /
 END
      ./bin/KiD_1D.exe namelists/${ampORbin[$iab]}_${bintype[$ibt]}.nml
