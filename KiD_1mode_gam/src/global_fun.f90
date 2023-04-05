b1953e10bc101aecc53a1c81dd9b1e37b5e540bb
Arthur <ols.Arthur@gmail.com>
2023-04-04 22:22:24 -0700

    add a linspace function

--- KiD_1mode_gam/src/global_fun.f90
+++ KiD_1mode_gam/src/global_fun.f90
@@ -517,4 +517,55 @@ End Subroutine mapRatio2R
 
 !}}}
 
+! ------------------------------------------
+! linspace: {{{
+! adopted from: https://gist.github.com/ivan-pi/f4b4741d7ed54ceff787c85d6ba22a5a
+
+function linspace(start,end,num,endpoint,step) result(samples)
+   ! PARAMETERS
+   real(8), intent(in) :: start 
+   !! The starting value of the sequence.
+   real(8), intent(in) :: end
+   !! The end value of the sequence, unless `endpoint` is set to `.false.`. 
+   !! In that case, the sequence consists of all but the last of `num + 1` 
+   !! evenly spaced samples, so that `end` is excluded. Note that the 
+   !! step size changes when `endpoint` is `.false.`.
+   integer, intent(in), optional :: num
+   !! Number of samples to generate. Default value is 50.
+   logical, intent(in), optional :: endpoint
+   !! If `.true.`, `end` is the last sample. Otherwise, it is not included. Default is `.true.`.
+   real(8), intent(out), optional :: step
+   !! If present, `step` is the size of spacing between samples.
+
+   ! RETURNS
+   real(8), allocatable :: samples(:)
+   !! There are `num` equally spaced samples in the closed interval `[start, stop]` or 
+   !! the half-open interval `[start, stop)` (depending on whether `endpoint` is `.true.` or `.false.`).
+
+   integer :: num_, i
+   logical :: endpoint_
+   real(8) :: step_
+
+   num_ = 50
+   if (present(num)) num_ = num
+
+   endpoint_ = .true.
+   if (present(endpoint)) endpoint_ = endpoint
+
+   ! find step size
+   if (endpoint_) then
+      step_ = (end - start)/real(num_-1,8)
+   else
+      step_ = (end - start)/real(num_,8)
+   end if
+
+   if (present(step)) step = step_
+
+   allocate(samples(num_))
+   do i = 1, num_
+      samples(i) = start + (i-1)*step_
+   end do
+end function linspace
+! }}}
+
 end module global_fun
