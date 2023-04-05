a4502b44237147910198c8a6300b409b1c184eb0
Arthur <ols.Arthur@gmail.com>
2023-04-04 22:19:17 -0700

    scripts for AMP p1

---
+++ KiD_1mode_gam/p1_archived/submit_alljobs4m.zsh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+sbatch job_condnuc4m
+sbatch job_collonly4m
+sbatch job_sedonly4m
+sbatch job_evaponly4m
+sbatch job_condcoll4m
+sbatch job_condcollsed4m
+sbatch job_fullmic4m
---
+++ KiD_1mode_gam/p1_archived/submit_autoconv.zsh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+
+sbatch job_collonly_vcm
+sbatch job_collonly_vcm_4m
+sbatch job_collonly_vdmr
+sbatch job_collonly_vdmr_4m
